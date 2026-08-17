b28cebc4bbb3d555f8570045e49193cb0b95d52c
Link4Electronics <link4electronics@gmail.com>
2026-08-17 18:53:59 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,9 +12,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common ffmpeg-mini
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
 	package=melonds-git
 else
