2eb7bc170d6a89a6e50aff99c474dc1fa714f995
Link4Electronics <link4electronics@gmail.com>
2026-08-17 18:52:34 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,15 +5,13 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/net.kuribo64.melonDS.png
 export DESKTOP=/usr/share/applications/net.kuribo64.melonDS.desktop
 
 # Deploy dependencies
 quick-sharun /usr/bin/melonDS
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
